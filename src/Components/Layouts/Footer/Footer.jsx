c5883932de3c261b9e6a63a57a8015fecae989a2
Dayveed021 <boskey021@gmail.com>
2023-12-16 19:17:34 -0600

    landingpage

--- src/Components/Layouts/Footer/Footer.jsx
+++ src/Components/Layouts/Footer/Footer.jsx
@@ -1,13 +1,68 @@
 import React from "react";
 import heroimg from "../../../assets/Images/HelloSiteF.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
     <footer>
-      <div className="left-side">
-        <img src={heroimg} alt="" />
-        <h2>Hivcbfdjk</h2>
+      <div className="footer-top">
+        <div className="left-side">
+          <img src={heroimg} alt="" />
+        </div>
+        <div className="right-side">
+          <section>
+            <header>COMPANY</header>
+            <ul>
+              <li>About</li>
+              <li>Careers</li>
+              <li>Investors</li>
+              <li>Partners</li>
+              <li>Legal</li>
+            </ul>
+          </section>
+          <section>
+            <header>SUPPORT</header>
+            <ul>
+              <li>Merchant Support</li>
+              <li>Help Center</li>
+              <li>Hire an expert</li>
+              <li>Community</li>
+            </ul>
+          </section>
+          <section>
+            <header>CONTACT US</header>
+            <ul>
+              <li>info@hellosite.com</li>
+              <li>1 - 800 - 200 - 300</li>
+              <li>1010 Sunset Blv. Palo Alto, California</li>
+            </ul>
+          </section>
+          <section>
+            <header>STAY UP TO DATE</header>
+            <span>Subscribe to Our newsletter</span>
+            <div className="subscribe">
+              <input type="email" placeholder=" Email" />
+              <FontAwesomeIcon
+                icon={faArrowRight}
+                color="#5729D4"
+                cursor="pointer"
+              />
+            </div>
+          </section>
+          <section>
+            <header>PRODUCTS</header>
+            <ul>
+              <li>Shop</li>
+              <li>Shop pay</li>
+              <li>Commerce Companies</li>
+            </ul>
+          </section>
+        </div>
       </div>
+      <hr />
+      <span className="copyrights">© Copyright Hellosite Inc.</span>
     </footer>
   );
 };
--- src/Components/Pages/LandingPage/LandingPage.jsx
+++ src/Components/Pages/LandingPage/LandingPage.jsx
@@ -3,9 +3,15 @@ import Navbar from "../../Layouts/Navbar/Navbar";
 import "./LandingPage.scss";
 import heroimage from "../../../assets/Images/hero-group.svg";
 import herolines from "../../../assets/Images/hero-lines.svg";
-import Footer from "../../Layouts/Footer/Footer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import sell1 from "../../../assets/Images/sell1.svg";
+import sell2 from "../../../assets/Images/sell2.svg";
+import sell3 from "../../../assets/Images/sell3.svg";
+import friendly from "../../../assets/Images/friendly.svg";
+import friendicon from "../../../assets/Icons/friend-icon.svg";
+import friendicon2 from "../../../assets/Icons/friend-icon2.svg";
+import Footer from "../../Layouts/Footer/Footer";
 
 const LandingPage = () => {
   return (
@@ -13,42 +19,84 @@ const LandingPage = () => {
       <Navbar />
       <div className="landing-page">
         <div className="hero">
-          <div className="hero-top-text">
-            <span>- HELLOSITE</span>
-            <h1>Unlock the Potential of Your Commerce.</h1>
-            <p>
-              HelloSite is championing the aspirations of up-and-coming
-              entrepreneurs, partnering with global giants, and nurturing
-              success for all those who find themselves somewhere in the vast
-              spectrum between.
-            </p>
+          <div className="hero-con">
+            <div className="hero-top-text">
+              <span>- HELLOSITE</span>
+              <h1>Unlock the Potential of Your Commerce.</h1>
+              <p>
+                HelloSite is championing the aspirations of up-and-coming
+                entrepreneurs, partnering with global giants, and nurturing
+                success for all those who find themselves somewhere in the vast
+                spectrum between.
+              </p>
+            </div>
+            <div className="hero-images">
+              <img src={herolines} alt="" className="img-floor" />
+              <img src={heroimage} alt="" className="hero-prototype" />
+            </div>
+            <div className="email-input">
+              <input type="email" placeholder="Email" />
+              <button>Get Early Access</button>
+            </div>
           </div>
-          <div className="hero-images">
-            <img src={herolines} alt="" className="img-floor" />
-            <img src={heroimage} alt="" className="hero-prototype" />
-          </div>
-          <div className="email-input">
-            <input type="email" placeholder="Email" />
-            <button>Get Early Access</button>
+          <div className="hero-footer">
+            <div className="hero-footer-con">
+              <div className="hero-footer-l">
+                <h2>Sell everything, anywhere to buyers</h2>
+              </div>
+              <div className="hero-footer-r">
+                <span>
+                  Sell online, in person, and around the world with the
+                  marketing tools, social integrations, and sales channels you
+                  need to get your products in front of customers.
+                </span>
+                <p>
+                  Start selling <FontAwesomeIcon icon={faArrowRight} bounce />
+                </p>
+              </div>
+            </div>
+            <div className="products">
+              <img src={sell3} alt="" />
+              <img src={sell2} alt="" />
+              <img src={sell1} alt="" />
+            </div>
           </div>
         </div>
-        <div className="hero-footer">
-          <div className="hero-footer-l">
-            <h2>Sell everything, anywhere to buyers</h2>
+        <div className="friendly">
+          <div className="friendly-img">
+            <img src={friendly} alt="" />
           </div>
-          <div className="hero-footer-r">
+          <div className="friendly-right">
+            <h2>User-Friendly setup</h2>
             <span>
-              Sell online, in person, and around the world with the marketing
-              tools, social integrations, and sales channels you need to get
-              your products in front of customers.
+              Easily create your online store without any technical expertise.
+              Our platform offers a simple and intuitive interface for store
+              setup.
             </span>
+            <div className="store">
+              <div className="online">
+                <img src={friendicon} alt="" />
+                <p>Online store builder</p>
+              </div>
+              <div className="online">
+                <img src={friendicon} alt="" />
+                <p>Theme store</p>
+              </div>
+            </div>
+
+            <ul>
+              <li>Select from a variety of customizable templates.</li>
+              <li>
+                With our drag and drop store you can bring your ideas to llife.
+              </li>
+            </ul>
             <p>
-              Start selling <FontAwesomeIcon icon={faArrowRight} bounce />
+              Learn More <FontAwesomeIcon icon={faArrowRight} />
             </p>
           </div>
         </div>
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
